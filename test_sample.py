a7fd1f2f1b924174e7e65d5b72334bfdf9d443e0
LAPTOP-DPS2T47K\user <s.jpstas@gmail.com>
2023-03-02 18:30:51 +0800

    updated

--- test_sample.py
+++ test_sample.py
@@ -1,3 +1,9 @@
+import requests
+import configparser
+
+config = configparser.ConfigParser()
+with open('config_file.ini') as fh:
+    config.read_file(fh)
 
 def test_one():
     print("this is to test")
@@ -8,3 +14,15 @@ def test_one():
 
     for i in range(1,100):
         print(i)
+
+def test_case1():
+    path1 = config.get('url_data', 'base_url')
+    resp = requests.get(f"{path1}api/users?page=2")
+    assert "200" in str(resp.status_code)
+
+
+def test_case2():
+    path1 = config.get('url_data', 'base_url')
+    resp = requests.post(f"{path1}api/users")
+    print(resp.text)
+    assert "201" in str(resp.status_code)
