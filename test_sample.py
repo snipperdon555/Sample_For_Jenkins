1f036b1597e4ab5c014b5bb2f3609abec7c210c4
LAPTOP-DPS2T47K\user <s.jpstas@gmail.com>
2023-03-02 18:17:32 +0800

    updated

--- test_sample.py
+++ test_sample.py
@@ -1,9 +1,10 @@
-print("this is to test")
-print("One More linse")
-a = 10
-b = 20
-print(a+b)
 
+def test_one():
+    print("this is to test")
+    print("One More linse")
+    a = 10
+    b = 20
+    print(a+b)
 
-for i in range(1,100):
-    print(i)
+    for i in range(1,100):
+        print(i)
